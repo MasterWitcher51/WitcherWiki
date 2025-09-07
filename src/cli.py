8e1aaf9c6be5556c16559847f071db805c846fcd
MasterWitcher51 <adamolatunji93@gmail.com>
2025-09-07 19:01:23 +0100

    CLI with initial functions

--- src/cli.py
+++ src/cli.py
@@ -1,1 +1,22 @@
 from bestiary import load_monsters
+
+def list_all_monsters():
+    # Function that list all monster names
+
+def show_details():
+    # Function that show details of monster by name
+
+def search_by_class():
+    # Function that searches for mosnters in a certain class
+
+def search_by_loot():
+    # Function that list monsters with a certain drop
+
+
+def main_menu():
+    print("Welcome to the Witcher 3 Bestiary App, select an option from below")
+    print("1. List all monsters")
+    print("2. Show details of monster")
+    print("3. Search by class")
+    print("4. Search by loot")
+    print("5. Exit")
